39681b80104a7c022d229261073fc031cca4e0f1
Straw <k7to.so23.ten717@i.softbank.jp>
2022-10-17 00:27:03 +0900

    外部パッケージ参照

--- variables-functions-packages/packages/helloworld/main.go
+++ variables-functions-packages/packages/helloworld/main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 
 	"github.com/myuser/calculator"
+	"rsc.io/quote"
 )
 
 func main() {
 	total := calculator.Sum(3, 5)
 	fmt.Println(total)
 	fmt.Println("Version: ", calculator.Version)
+	fmt.Println(quote.Hello())
 }
